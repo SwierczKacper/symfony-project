4094beb32b94daa1dfebbef259a4408b4d067782
SwierczKacper <swierczkacperkontakt@gmail.com>
2024-01-08 17:11:50 +0100

    Add email sending after user account creation

--- src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
+++ src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Core\User\Application\Command\CreateUser;
 
+use App\Common\Mailer\MailerInterface;
 use App\Core\User\Domain\Repository\UserRepositoryInterface;
 use App\Core\User\Domain\User;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -10,15 +11,22 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 class CreateUserHandler
 {
 	public function __construct(
-		private readonly UserRepositoryInterface $userRepository
+		private readonly UserRepositoryInterface $userRepository,
+		private readonly MailerInterface $mailer,
 	) {}
 	
 	public function __invoke(CreateUserCommand $command): void
 	{
 		//TODO: add validation if user with provided email already exists
 		
-		$this->userRepository->save(new User(
-			$command->email
-		));
+		$user = new User($command->email);
+		$this->userRepository->save($user);
+		
+		// Send email notification
+		$this->mailer->send(
+			$user->getEmail(),
+			'Rejestracja konta',
+			'Zarejestrowano konto w systemie. Aktywacja konta trwa do 24h'
+		);
 	}
 }
